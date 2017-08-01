74ad6147e773bc342866a1394fbbe11553908d26
lf.kushinn@gmail.com <lf.kushinn@gmail.com>
2017-08-01 15:51:29 +0800

    add cmds

---
+++ cmd/vs2013install.bat
@@ -0,0 +1,1 @@
+"D:\Program Files\Microsoft Visual Studio 12.0\Common7\IDE\VSIXInstaller.exe" ShaderlabVS.vsix
---
+++ cmd/vs2015install.bat
@@ -0,0 +1,1 @@
+"D:\Program Files\Microsoft Visual Studio 14.0\Common7\IDE\VSIXInstaller.exe" ShaderlabVS.vsix
